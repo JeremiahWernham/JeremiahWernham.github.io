0411dff8094b2b08bfc5141b6eb905224620062d
Jeremiah Wernham <jeremiah.wernham@gmail.com>
2023-01-18 23:21:10 -0500

    Update wording

---
+++ webp.js
@@ -0,0 +1,9 @@
+/**********************************/
+/*  Author: Jeremiah Wernham      */
+/*  Date: 1/18/2023               */
+/*  File: webp.js                 */
+/*  Description: A collection of  */
+/*  projects in JavaScript        */
+/*  Dependencies: none            */
+/**********************************/
+
